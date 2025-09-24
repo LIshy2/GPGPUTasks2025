2b03e5c3b03c49d5dd3d2df8d2dcc44157db5d14
Islam Talipov <ishaykhiev2017@litsey2.ru>
2025-09-24 23:26:22 +0300

    anchugan

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,8 @@ int main()
 		// Не забывайте проверять коды ошибок с помощью макроса OCL_SAFE_CALL
 		size_t platformNameSize = 0;
 		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, 0, nullptr, &platformNameSize));
-		// TODO 1.1
+		// 1.1
+		// // ну я поставил 10 оно упало и все
 		// Попробуйте вместо CL_PLATFORM_NAME передать какое-нибудь случайное число - например 239
 		// Т.к. это некорректный идентификатор параметра платформы - то метод вернет код ошибки
 		// Макрос OCL_SAFE_CALL заметит это, и кинет ошибку с кодом
@@ -67,27 +68,54 @@ int main()
 		// в документации подробно объясняется, какой ситуации соответствует данная ошибка, и это позволит, проверив код, понять, чем же вызвана данная ошибка (некорректным аргументом param_name)
 		// Обратите внимание, что в этом же libs/clew/CL/cl.h файле указаны всевоможные defines, такие как CL_DEVICE_TYPE_GPU и т.п.
 
-		// TODO 1.2
+		// 1.2
 		// Аналогично тому, как был запрошен список идентификаторов всех платформ - так и с названием платформы, теперь, когда известна длина названия - его можно запросить:
 		std::vector<unsigned char> platformName(platformNameSize, 0);
-		// clGetPlatformInfo(...);
+
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, platformNameSize, platformName.data(), &platformNameSize));
 		std::cout << "    Platform name: " << platformName.data() << std::endl;
 
-		// TODO 1.3
+		// 1.3
 		// Запросите и напечатайте так же в консоль вендора данной платформы
+		char vendorName[20];
+		size_t vendorNameSize;
+		OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, 20, vendorName, &vendorNameSize));
+		std::cout << "    Vendor name: " << vendorName << std::endl;
 
-		// TODO 2.1
+		// 2.1
 		// Запросите число доступных устройств данной платформы (аналогично тому, как это было сделано для запроса числа доступных платформ - см. секцию "OpenCL Runtime" -> "Query Devices")
 		cl_uint devicesCount = 0;
 
+		OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, nullptr, &devicesCount));
+
+		std::vector<cl_device_id> devices(devicesCount);
+
+		OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, devicesCount, devices.data(), &devicesCount));
+
+        std::cout << "    Number of devices: " << devicesCount << std::endl;
 		for(int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex)
 		{
-			// TODO 2.2
-			// Запросите и напечатайте в консоль:
-			// - Название устройства
-			// - Тип устройства (видеокарта/процессор/что-то странное)
-			// - Размер памяти устройства в мегабайтах
-			// - Еще пару или более свойств устройства, которые вам покажутся наиболее интересными
+		    char deviceName[256];
+            cl_device_type type;
+            cl_ulong globalMem;
+            cl_uint computeUnits;
+            cl_uint freq;
+            size_t maxWG;
+
+            clGetDeviceInfo(devices[deviceIndex], CL_DEVICE_NAME, sizeof(deviceName), deviceName, NULL);
+            clGetDeviceInfo(devices[deviceIndex], CL_DEVICE_TYPE, sizeof(type), &type, NULL);
+            clGetDeviceInfo(devices[deviceIndex], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(globalMem), &globalMem, NULL);
+            clGetDeviceInfo(devices[deviceIndex], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(computeUnits), &computeUnits, NULL);
+            clGetDeviceInfo(devices[deviceIndex], CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(freq), &freq, NULL);
+            clGetDeviceInfo(devices[deviceIndex], CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(maxWG), &maxWG, NULL);
+
+            std::cout << "    Device #" << deviceIndex + 1 << "/" << devicesCount << std::endl;
+            std::cout << "        Device name: " << deviceName << std::endl;
+            std::cout << "        Device type: " << type << std::endl;
+            std::cout << "        Global memory size: " << globalMem << std::endl;
+            std::cout << "        Compute units: " << computeUnits << std::endl;
+            std::cout << "        Max frequency: " << freq << std::endl;
+            std::cout << "        Max work group: " << maxWG << std::endl;
 		}
 	}
 
